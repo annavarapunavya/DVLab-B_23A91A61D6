8cbc15d32ec6ecb7b0df55a63560a6b67b4b6ba9
Navya <annavarapunavya2005@gmail.com>
2026-02-25 19:46:05 +0530

    Added labels

--- scripts/1b_airquality(histogram).R
+++ scripts/1b_airquality(histogram).R
@@ -5,3 +5,13 @@ View(airquality)
 
 #Basic histogram
 hist(airquality$Temp)
+
+#Adding labels and colours
+hist(airquality$Temp,
+     breaks=5,
+     main= "Basic Histogram of Air Quality",
+     xlab= "Temperature",
+     ylab= "Range",
+     labels=TRUE,
+     col = "lightblue",
+     border = "darkblue")
